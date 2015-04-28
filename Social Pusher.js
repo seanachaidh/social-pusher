e7955ff39988984b85877d26dade636d4ed31716
Elias Schroons <elias@mymicroinvest.com>
2015-04-28 14:27:12 +0200

    Initial

---
+++ Social Pusher.js
@@ -0,0 +1,13 @@
+/*
+ * Prints all the tweets from the list.
+ */
+
+var tweets = [
+  "To be or not to be. -- Sharekspeare",
+  "Ship it -- some programmer",
+  "Very few have succeeded -- Elias"
+];
+
+for(var i=0; i<tweets.length; i++) {
+  console.log(tweets[i]);
+}
