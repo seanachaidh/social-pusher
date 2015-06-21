cf6daec67acdcaa68acc876813e76f3c930b8576
seanachaidh <pvankeymeulen@seanachaidh.be>
2015-06-21 21:37:05 +0200

    Extra code toevoegen

--- social_pusher.js
+++ social_pusher.js
@@ -2,6 +2,9 @@
  * Prints all the tweets from the list.
  */
 
+/*
+Code of Elias Schroons
+
 var tweets = [
   "To be or not to be. -- Sharekspeare",
   "Ship it -- some programmer",
@@ -11,3 +14,26 @@ var tweets = [
 for(var i=0; i<tweets.length; i++) {
   console.log(tweets[i]);
 }
+*/
+
+var TwitterApi = require('twitter-node-client').Twitter;
+
+var success = function(data) {
+	console.log("Error: [%s]", data);
+};
+
+var error = function(data) {
+	console.log("Data: [%s]", data);
+};
+
+var config = [
+	"consumerKey": "XXX",
+	"consumerSecret": "XXX",
+	"accessToken": "XXX",
+	"accessTokenSecret": "XXX",
+	"callBackUrl": "XXX"
+];
+
+var twitter = new TwitterApi(config);
+
+twitter.getUserTimeline({screen_name: "Seanachaidh", count: '10'}, error, success);
